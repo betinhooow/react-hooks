c834d0be1e3aff52c0970b65852189e50192a486
Roberto Nobre <robertonobre90@gmail.com>
2019-03-27 17:44:49 -0300

    Starting with hooks from docs, a little experience

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+export default function App(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/robertonobre/repos');
+    const data = await response.json();
+    setRepositories(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered =  repositories.filter(item => item.favorite);
+    document.title = `Você tem ${filtered.length} favoritos.`;
+  }, [repositories]);
+
+  function handleFavorite(id){
+    const reps = repositories.map(item => 
+      item.id == id ? { ...item, favorite: !item.favorite} : item);
+
+    setRepositories(reps);
   }
-}
 
-export default App;
+  function handleEdit(id){
+    const reps = repositories.map(item =>
+      item.id == id ? { ...item, isEdit: !item.isEdit } : item);
+
+    setRepositories(reps);
+  }
+
+  function handleText(e, id){
+    const reps = repositories.map(item =>
+      item.id == id ? { ...item, name: e.target.value } : item);
+    setRepositories(reps);
+  }
+
+  return (
+    <>
+      <table>
+        <tr>
+          <td><b>Repository name</b></td>
+        </tr>
+        {repositories.map(item => 
+          <tr key={item.id}>
+            <td>
+              {item.isEdit ? 
+                <input 
+                  type="text" 
+                  name="repo" 
+                  value={item.name} 
+                  onChange={(e) => handleText(e, item.id)}/> 
+                : item.name} {item.favorite && <span>(Favorito) </span>}
+            </td>
+            <td>
+              <button 
+                onClick={() => handleEdit(item.id)}>
+                {!item.isEdit ? `Edit` : `Save`}
+              </button>
+            </td>
+            <td>
+              <button onClick={() => handleFavorite(item.id)}>Favoritar</button>
+            </td>
+          </tr>
+          )}
+      </table>
+    </>
+  );
+}
